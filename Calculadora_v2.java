f378dd55c5fe4520982d9446f7e8a58979a57516
Inacio Menezes <menezesoliveira1011@gmail.com>
2023-11-22 10:59:21 -0300

    Projeto Calculadora

---
+++ Calculadora_v2.java
@@ -0,0 +1,27 @@
+public class Calculadora_v2 {
+
+    //Método para somar os dois números:
+    
+       public int somar(int a, int b) {
+           return a + b;
+       }
+    
+    //Método para subtrair os dois números:
+    
+       public int subtrair(int a, int b) {
+           return a - b;
+       }
+    
+       public static void main(String[] args) {
+    
+    //Exemplo de uso da classe:
+    
+           Calculadora_v2 calculadora = new Calculadora_v2();
+    
+           int resultadoSoma = calculadora.somar(5, 3);
+           System.out.println("Resultado da soma: " + resultadoSoma);
+    
+           int resultadoSubtracao = calculadora.subtrair(10, 4);
+           System.out.println("Resultado da subtração: " + resultadoSubtracao);
+       }
+    }
